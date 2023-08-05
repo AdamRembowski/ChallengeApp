100e82bac1bd36bfdfd9718f1e9b21fe408ece7b
AdamRembowski <adam.rembowski@o2.pl>
2023-08-05 11:34:03 +0200

    Zadanie dzień czwarty

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Name = "Adam";
+bool IsMan = true;
+int Age = 37;
+
+if (IsMan && Age >= 18)
+{
+    Console.WriteLine("Pełnoletni Mężczyzna");
+}
+else if (!IsMan && Age >= 18)
+{
+    Console.WriteLine("Pełnoletnia Kobieta");
+}
+else
+{
+    Console.WriteLine("Niepełnoletnie dziecko");
+}
+
+if (IsMan && Age < 30)
+{
+    Console.WriteLine("Mężczyzna poniżej 30 lat");
+}
+else if (!IsMan && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if (Name == "Adam" && Age == 37)
+{
+    Console.WriteLine(Name + ", lat " + Age);
+}
+else
+{
+    Console.WriteLine("ktoś inny");
+}
