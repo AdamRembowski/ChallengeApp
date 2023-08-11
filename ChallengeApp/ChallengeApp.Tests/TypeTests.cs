76a34627b16c3558b6c028d79dd999ef7d2eb6b3
AdamRembowski <adam.rembowski@o2.pl>
2023-08-11 12:37:03 +0200

    Zadanie dzien 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TestReferenceAndStringType()
+        {
+            //arrange
+            var user = new Employee("Adam", "Rembowski", 37);
+            string data = "Adam Rembowski";
+
+            //act
+
+            //assert
+            Assert.AreEqual(user.Name+" "+user.Surname, data);
+        }
+        public void TestValueType()
+        {
+            //arrange
+            int integer = 5;
+            long longint = 5;
+            float floatint = 5;
+            List<int> ints = new List<int>();
+
+            //act
+            ints.Add(5);
+
+            //assert
+            Assert.AreEqual(integer, longint);
+            Assert.AreEqual(longint, floatint);
+            Assert.AreEqual(integer, floatint);
+            Assert.AreEqual(ints[0], integer);
+
+        }
+    }
+}
